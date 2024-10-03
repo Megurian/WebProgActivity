8d44401d70632c8c8418d72d0d5ae95bb241009d
Megurian <eh202201078@wmsu.edu.ph>
2024-10-03 22:09:55 +0800

    Created Signup form and functionality

--- signup.php
+++ signup.php
@@ -25,16 +25,16 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     //Validation Last name
     if(empty($last_name)) {
-        $last_nameE = 'First name is required to signup';
+        $last_nameE = 'Last name is required to signup';
     } elseif(is_numeric($last_name)) {
-        $last_nameE = 'Enter a valid first name';
+        $last_nameE = 'Enter a valid Last name';
     }
 
      //Validation username
      if(empty($username)) {
-        $usernameE = 'First name is required to signup';
+        $usernameE = 'Username is required to signup';
     } elseif(is_numeric($username)) {
-        $usernameE = 'Enter a valid first name';
+        $usernameE = 'Enter a valid username';
     }
 
     //Validation Password
@@ -42,8 +42,15 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         $passwordE = 'Password is required to signup';
     } elseif(strlen($password) < 8) {
         $passwordE = 'Enter atleast 8 characters password';
-    } elseif($password == $first_name || $password == $last_name || $password == $username) {
-        $passwordE = 'Weak password';
+    } elseif(!preg_match('/[0-9]/', $password) || 
+             !preg_match('/[A-Z]/', $password) || 
+             !preg_match('/[a-z]/', $password) || 
+             !preg_match('/[^a-zA-Z\d]/', $password)) {
+        $passwordE = 'Password must contain at least 1 number, 1 uppercase, 1 lowercase, 1 special';
+    } elseif(strpos($password, $first_name) !== false || 
+             strpos($password, $last_name) !== false || 
+             strpos($password, $username) !== false) {
+        $passwordE = 'Weak password, please try a different password';
     } elseif(empty($confirm_password)) {
         $confirm_passwordE = 'Please confirm your password';
     } elseif($confirm_password != $password) {
@@ -53,10 +60,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     if(empty($usernameE) && empty($first_nameE) && empty($last_nameE) &&
        empty($passwordE) && empty($confirm_passwordE)) {
         
-        $options = [
-            'cost' => 15    //this set the computational expense
-        ];
-        $encrytedPassword = password_hash($confirm_password, PASSWORD_DEFAULT, $options);
+        $encrytedPassword = password_hash($confirm_password, PASSWORD_DEFAULT, ['cost' => 12]);
 
         $accountObj->username = $username;
         $accountObj->first_name = $first_name;
@@ -82,54 +86,50 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Signup</title>
-    <style>
-        * {
-            align-items: center;
-        }
-    </style>
+    <link rel="stylesheet" href="https://classless.de/classless.css">
 </head>
 <body>
     <form action="" method="post">
-        <h2>Staff Sign-up</h2>
+        <h2>Customer Sign-up</h2>
         <p>All field are required</p>
-        <hr><br><br>
+        <hr><br>
 
         <label for="first_name">Enter your first name:</label><br>
         <input type="text" name="first_name" placeholder="Enter first name:" value="<?php echo $first_name; ?>">
         <?php if(!empty($first_nameE)): ?>
             <span class="error" style="color: red;"><?= $first_nameE ?></span><br>
         <?php endif; ?>
-        <br><br>
+        <br>
 
         <label for="last_name">Enter your last name:</label><br>
         <input type="text" name="last_name" placeholder="Enter last name:" value="<?php echo $last_name; ?>">
         <?php if(!empty($last_nameE)): ?>
             <span class="error" style="color: red;"><?= $last_nameE ?></span><br>
         <?php endif; ?>
-        <br><br>
+        <br>
 
         <label for="username">Enter your username:</label><br>
         <input type="text" name="username" placeholder="Enter your username:" value="<?php echo $username; ?>">
         <?php if(!empty($usernameE)): ?>
             <span class="error" style="color: red;"><?= $usernameE ?></span><br>
         <?php endif; ?>
-        <br><br>
+        <br>
         
         <label for="password">Create password:</label><br>
         <input type="password" id="password" name="password" placeholder="Enter at least 8 characters password">
         <?php if(!empty($passwordE)): ?>
             <span class="error" style="color: red;"><?= $passwordE ?></span><br>
         <?php endif; ?>
-        <br><br>
+        <br>
 
         <label for="confirm_password">Confirm your password</label><br>
         <input type="password" name="confirm_password" placeholder="Confirm your password">
         <?php if(!empty($confirm_passwordE)): ?>
             <span class="error" style="color: red;"><?= $confirm_passwordE ?></span><br>
         <?php endif; ?>
-        <br><br>
+        <br>
 
-        <input type="submit" value="Sign-up" style="background-color: pink;">
+        <input type="submit" value="Sign-up" style="background-color: green;">
     </form>
 </body>
 </html>
